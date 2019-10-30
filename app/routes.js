4b9bcd3cd095c9e618fe202390792a600d01d9cf
justin <jlouie10@gmail.com>
2019-10-30 14:27:13 -0500

    Add roles methods with basic CRUD operations

--- app/routes.js
+++ app/routes.js
@@ -3,9 +3,11 @@
 const router = require('express').Router(); // Create a Router instance
 
 // Require direct paths to avoid cirular dependencies
+const roles = require('./roles/rolesRoutes');
 const users = require('./users/usersRoutes');
 
 // Routes
+router.use('/roles', roles);
 router.use('/users', users);
 
 module.exports = router;
