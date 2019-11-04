8e638d90f25329bca37c3213eec40d994bcd5f4a
justin <jlouie10@gmail.com>
2019-11-04 13:11:29 -0600

    Add invalid request error to public routes

--- app/login.js
+++ app/login.js
@@ -11,4 +11,19 @@ router
   .post(user.login, session.create, user.attach)
   .delete(session.logout, user.attach);
 
+// Handle invalid requests
+router.use(invalidRequest);
+
+/**
+ * Error that is raised when a request is initiated with invalid parameters
+ */
+function invalidRequest(req, res) {
+  res.status(404).json({
+    error: {
+      code: 404,
+      message: `Unrecognized request URL (${req.method}: ${req.originalUrl})`
+    }
+  });
+}
+
 module.exports = router;
